70c7079958482902c8105063ba68cb6b6e7cf600
Aleksander <64606852+Alex-VGB@users.noreply.github.com>
2020-09-12 14:48:14 +0200

    Connected React Router. Made deletion, editing

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,43 +1,58 @@
 import React from 'react'
 import classNames from 'classnames'
+import axios from 'axios'
 
-import './List.scss'
-import Badge from "../Badge";
 import removeSvg from '../../assets/img/remove.svg'
 
-const List = ({items, isRemovable, onClick, onRemove}) => {
+import Badge from '../Badge'
 
-    const removeList = (item) => {
-        if (window.confirm('You really want to delete list?')) {
-            onRemove(item)
+import './List.scss'
+
+const List = ({
+                  items,
+                  isRemovable,
+                  onClick,
+                  onRemove,
+                  onClickItem,
+                  activeItem
+              }) => {
+    const removeList = item => {
+        if (window.confirm('Are you sure you want to delete the list?')) {
+            axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
+                onRemove(item.id)
+            })
         }
     }
 
     return (
-        <ul
-            onClick={onClick}
-            className='list'>
-            {
-                items.map((item, index) => (
-                        <li key={index}
-                            className={classNames(item.className, {active: item.active})}>
-                            <i>
-                                {item.icon
-                                    ? item.icon
-                                    : <Badge color={item.color}/>
-                                }
-                            </i>
-                            <span>{item.name}</span>
-                            {isRemovable && <img
-                                onClick={() => removeList(item)}
-                                className='list__remove-icon'
-                                src={removeSvg}
-                                alt='Remove'/>}
-                        </li>
-                    )
-                )
-            }
+        <ul onClick={onClick} className="list">
+            {items.map((item, index) => (
+                <li
+                    key={index}
+                    className={classNames(item.className, {
+                        active: item.active
+                            ? item.active
+                            : activeItem && activeItem.id === item.id
+                    })}
+                    onClick={onClickItem ? () => onClickItem(item) : null}
+                >
+                    <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
+                    <span>
+            {item.name}
+                        {item.tasks && ` (${item.tasks.length})`}
+          </span>
+                    {isRemovable && (
+                        <img
+                            className="list__remove-icon"
+                            src={removeSvg}
+                            alt="Remove icon"
+                            onClick={() => removeList(item)}
+                        />
+                    )}
+                </li>
+            ))}
         </ul>
     )
 }
+
 export default List
